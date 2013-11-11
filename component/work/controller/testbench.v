165af98b5ab04f3bde6400f497d94fcdb7738e3f
unknown <fstan@eecs373-05.adsroot.itcs.umich.edu>
2013-11-11 18:28:36 -0500

    read signal not working. Is always evalutating to true. However, we are able to see the data from gcc after sending the poll command.

---
+++ component/work/controller/testbench.v
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////////
+// Created by Actel SmartDesign Mon Nov 11 17:44:22 2013
+// Testbench Template
+// This is a basic testbench that instantiates your design with basic 
+// clock and reset pins connected.  If your design has special
+// clock/reset or testbench driver requirements then you should 
+// copy this file and modify it. 
+//////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module testbench;
+
+parameter SYSCLK_PERIOD = 100; // 10MHz
+
+reg SYSCLK;
+reg NSYSRESET;
+
+initial
+begin
+    SYSCLK = 1'b0;
+    NSYSRESET = 1'b0;
+end
+
+//////////////////////////////////////////////////////////////////////
+// Reset Pulse
+//////////////////////////////////////////////////////////////////////
+initial
+begin
+    #(SYSCLK_PERIOD * 10 )
+        NSYSRESET = 1'b1;
+end
+
+
+//////////////////////////////////////////////////////////////////////
+// 10MHz Clock Driver
+//////////////////////////////////////////////////////////////////////
+always @(SYSCLK)
+    #(SYSCLK_PERIOD / 2.0) SYSCLK <= !SYSCLK;
+
+
+//////////////////////////////////////////////////////////////////////
+// Instantiate Unit Under Test:  controller
+//////////////////////////////////////////////////////////////////////
+controller controller_0 (
+    // Inputs
+    .contREAD({1{1'b0}}),
+
+    // Outputs
+    .contWRITE( ),
+    .RDATA( )
+
+    // Inouts
+
+);
+
+endmodule
+
